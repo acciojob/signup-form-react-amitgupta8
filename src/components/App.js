3110ef95133b6e3be9c60b27a228602ea17ef186
Amit Gupta <amit.the.coder404@gmail.com>
2023-04-28 21:26:20 +0530

    submitting code-Fri Apr 28 2023 21:26:18 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import React from './SingnupFrom'
+import SignupForm from "./SingnupFrom";
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <SignupForm/>
+    </div>
   )
 }
 
